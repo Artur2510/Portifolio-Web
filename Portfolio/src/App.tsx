b26d853e4e3cf72d3a5cc3248b842192ac3d90e5
usuario <arturamosbicalho@hotmail.com>
2024-09-04 20:50:50 -0300

    Estruturando-Portfolio

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,20 @@
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Projects from "./components/Projects"
+import Tecnologies from "./components/Tecnologies"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <About />
+      <Projects />
+      <Tecnologies />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio/src/components/Projects.tsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return(
+      <div className="class-projects">
+        <h2>PROJETOS</h2>
+        <p>ELABORAR COMO COLOCAR OS PROJETOS AQUI</p>
+      </div>
+    )
+  };
+  
+  export default Projects;
